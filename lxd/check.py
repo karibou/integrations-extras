68ec739c2cabfdf876a6b392b9af1fdf6d424c12
Louis Bouchard <louis@ubuntu.com>
2017-05-23 16:57:33 +0200

    [lxd] PEP8/Flake8 cleanup
    Signed-off-by: Louis Bouchard <louis@ubuntu.com>

--- lxd/check.py
+++ lxd/check.py
@@ -4,7 +4,6 @@ import requests_unixsocket
 import urllib
 
 from checks import AgentCheck
-from hashlib import md5
 
 
 class LXDCheck(AgentCheck):
@@ -28,30 +27,30 @@ class LXDCheck(AgentCheck):
                 'timestamp': int(time.time()),
                 'event_type': 'lxd_check',
                 'msg_title': 'LXD CONN',
-                'msg_text': 'Unable to connect to LXD daemon : %s' %e
+                'msg_text': 'Unable to connect to LXD daemon : %s' % e
             })
             return False
 
     def _query_lxd(self, data_element):
         if self.lxd_api is None:
-	    self._connect_local_client()
+            self._connect_local_client()
 
-	lxd_data = self.lxd_api.get('%s%s' % (self.baseURL, data_element))
-	if lxd_data.json()['status_code'] == 200:
-	    return lxd_data.json()['metadata']
-	else:
-	    return None
-
+        lxd_data = self.lxd_api.get('%s%s' % (self.baseURL, data_element))
+        if lxd_data.json()['status_code'] == 200:
+            return lxd_data.json()['metadata']
+        else:
+            return None
 
     def get_containers(self):
-	container_data = self._query_lxd(self.containers)
+        container_data = self._query_lxd(self.containers)
         self.container_list = [c.split('/')[-1] for c in container_data]
         return self.container_list
 
-    def get_container_statistics(self, container):
-        if container:
-            self.statistics[container] = self._query_lxd(self.stats % container)
+    def get_container_statistics(self, containr):
+        if containr:
+            self.statistics[containr] = self._query_lxd(self.stats % containr)
             return self.statistics
+
 
 if __name__ == '__main__':
     check, instances = LXDCheck.from_yaml('/etc/dd-agent/conf.d/lxd.yaml')
