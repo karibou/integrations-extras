32f152bc2e1ea42b911870feebd0cd76f8fd04e9
Louis Bouchard <louis@ubuntu.com>
2017-06-05 17:53:53 +0200

    [lxd] Remove cruft from the test template
    Signed-off-by: Louis Bouchard <louis@ubuntu.com>

--- lxd/test_lxd.py
+++ lxd/test_lxd.py
@@ -1,24 +1,11 @@
-# (C) Datadog, Inc. 2010-2016
-# All rights reserved
-# Licensed under Simplified BSD License (see LICENSE)
-
 # stdlib
 from nose.plugins.attrib import attr
-
-# 3p
 
 # project
 from tests.checks.common import AgentCheckTest
 
 
-instance = {
-    'host': 'localhost',
-    'port': 26379,
-    'password': 'datadog-is-devops-best-friend'
-}
 
-
-# NOTE: Feel free to declare multiple test classes if needed
 
 @attr(requires='lxd')
 class TestLxd(AgentCheckTest):
